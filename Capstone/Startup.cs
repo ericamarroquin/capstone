df091343b1fab5dee21b37d49099986357276ee3
EricaMarroquin <ericamarroquin03@gmail.com>
2021-12-16 16:24:08 -0800

    add updated Startup file and appsettings.json

--- Capstone/Startup.cs
+++ Capstone/Startup.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Capstone.Models;
 
 namespace Capstone
 {
@@ -26,12 +20,10 @@ namespace Capstone
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<CapstoneContext>(opt=>
+                opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"],
+                ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Capstone", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,11 +32,9 @@ namespace Capstone
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Capstone v1"));
             }
 
-            // app.UseHttpsRedirection();
+            // app.UseHttpsRedirection(); // redirects to https port - increases security but slows down development
 
             app.UseRouting();
 
