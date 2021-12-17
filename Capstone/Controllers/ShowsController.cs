cebc6c0b37acdbb12ec8041c292abaeabf1bac97
EricaMarroquin <ericamarroquin03@gmail.com>
2021-12-16 17:18:25 -0800

    add ShowsController with GET and POST routes

---
+++ Capstone/Controllers/ShowsController.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Capstone.Models;
+
+namespace Capstone.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ShowsController : ControllerBase
+  {
+    private readonly CapstoneContext _db;
+
+    public ShowsController(CapstoneContext db)
+    {
+      _db = db; // allows us to use private database as public 
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Show>>> Get()
+    {
+      return await _db.Shows.ToListAsync();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Show>> Post(Show show)
+    {
+      _db.Shows.Add(show);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction("Post", new { id = show.ShowId }, show);
+    }
+  }
+}
