1da7e23263e78031b1f38e9a5c4f3e977c18b136
EricaMarroquin <ericamarroquin03@gmail.com>
2021-12-22 08:53:27 -0800

    add migration to update join entities

--- Capstone/Migrations/20211222164047_JoinEntitiesUpdate.Designer.cs
+++ Capstone/Migrations/20211222164047_JoinEntitiesUpdate.Designer.cs
@@ -2,14 +2,16 @@
 using Capstone.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Capstone.Migrations
 {
     [DbContext(typeof(CapstoneContext))]
-    partial class CapstoneContextModelSnapshot : ModelSnapshot
+    [Migration("20211222164047_JoinEntitiesUpdate")]
+    partial class JoinEntitiesUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
